0108438ef4a92b915989557ae1c814805edcaeb5
loomkoom <29258685+loomkoom@users.noreply.github.com>
2022-06-15 20:13:21 +0200

    .

--- script.py
+++ script.py
@@ -89,10 +89,10 @@ def changes(resp):
             print('--------------------------------------')
             print('since: ', resp.body.since_change_number)
             print('current: ', change_number)
-            print(app_changes)
+            # print(app_changes)
             appids = [app.appid for app in app_changes]
             ret = client.get_product_info(apps=appids, auto_access_tokens=True)
-            print('\n',ret)
+            # print('\n',ret)
             for appid in appids:
                 app = ret['apps'][appid]
                 try:
