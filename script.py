7a3ec8fcc833b577c8a849c6f7f9b3ed943619a6
loomkoom <29258685+loomkoom@users.noreply.github.com>
2022-06-18 19:44:58 +0200

    .

--- script.py
+++ script.py
@@ -45,9 +45,10 @@ def logon():
     if client.user.name is None:
         LOG.debug("waiting for account info")
         client.wait_msg(EMsg.ClientAccountInfo, timeout=2)
-    LOG.info(f"Logged on as: {client.user.name}")
-    LOG.info('--------------------------------------')
-    client.get_changes_since(change_number-1)
+    else:
+        LOG.info(f"Logged on as: {client.user.name}")
+        LOG.info('--------------------------------------')
+        client.get_changes_since(change_number - 1)
 
 
 @client.on("connected")
